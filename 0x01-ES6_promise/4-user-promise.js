48dff06e017244c0a60c2398341b6bc6f9d81062
njdam <jeandamngaboyimana@gmail.com>
2024-03-06 19:07:02 +0000

    4. Simple promise!

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,6 @@
 // A Simple promise
 export default function signUpUser(firstName, lastName) {
+  // or return Promise.resolve({ firstName, lastName });
   return new Promise((resolve, reject) => {
     if (firstName && lastName) {
       resolve({ firstName, lastName });
