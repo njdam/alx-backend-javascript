10801f5636ab1eb7989fc4724bec74d0fcad0c92
njdam <jeandamngaboyimana@gmail.com>
2024-03-06 17:09:57 +0000

    Don't make a promise...if you know you can't keep it.

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+// Don't make a promise...if you know you can't keep it
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
