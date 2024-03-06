35066ca2728a4c1c0d689dcf2af6990948c58fdf
njdam <jeandamngaboyimana@gmail.com>
2024-03-06 19:50:51 +0000

    7. Load balancer - Promise.race!

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,5 @@
+// Load balancer should accept two arguments chinaDownload (Promise)
+// and USDownload (Promise).
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
+}
