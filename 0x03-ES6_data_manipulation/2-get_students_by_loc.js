0330f2b394f91d73a91b7840645f3bb1d78769e3
njdam <jeandamngaboyimana@gmail.com>
2024-03-11 22:53:07 +0000

    Filter - A function that returns an array of objects who are located in a specific city!

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,5 @@
+// Filter - A function that returns an array of objects
+// who are located in a specific city
 export default function getStudentsByLocation(studentsInfo, myLocation) {
   if (!Array.isArray(studentsInfo) || !myLocation) {
     return [];
