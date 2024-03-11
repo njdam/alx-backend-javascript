a8c4e794e087ebbcb13f972cb1c873a900121f5a
njdam <jeandamngaboyimana@gmail.com>
2024-03-11 22:57:45 +0000

    Combine - A function that returns an array of students for a specific city with their new grade!

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,15 @@
+/* Combine - A function that returns an array of students
+ * for a specific city with their new grade
+ */
 export default function updateStudentGradeByCity(students, city, newGrades) {
   let studentsAtCity = [];
 
   if (Array.isArray(students) && city && Array.isArray(newGrades)) {
     studentsAtCity = students
-      /* The filter method returns an array containing all elements */
+      // The filter method returns an array containing all elements
       .filter((student) => student.location === city)
       .map((student) => {
-        /* The find method is used, which returns first element in array */
+        // The find method is used, which returns first element in array
         const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
         return {
           ...student,
