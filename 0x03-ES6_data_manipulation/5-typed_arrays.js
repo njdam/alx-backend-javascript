4ae2fe72afaef2ddb145e2cdec27ecb06165df12
njdam <jeandamngaboyimana@gmail.com>
2024-03-11 23:02:39 +0000

    TypeArrays - A function that returns a new ArrayBuffer with an Int8 value at a specific position!

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,15 @@
+/* TypeArrays - A function that returns a new ArrayBuffer
+ * with an Int8 value at a specific position
+ */
 export default function createInt8TypedArray(length, position, value) {
+  // Handlering the index error
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
 
+  // Creating New Array buffer
   const buffer = new ArrayBuffer(length);
+  // Setting buffer as an Int8 array and assign it to a value
   const int8Array = new Int8Array(buffer);
   int8Array[position] = value;
 
