f9ba2bfe6179eda59f06bb1764ce3984fa6be13e
njdam <jeandamngaboyimana@gmail.com>
2024-03-13 00:02:05 +0000

    Weak link data structure - A function that track within the weakMap the number of times queryAPI is called for each endpoint!

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -5,29 +5,19 @@ export const weakMap = new WeakMap();
  * Weak link data structure - A function which track within the weakMap
  * the number of times queryAPI is called for each endpoint
  *
- * @param {
+ * @param {{
  *   protocol: 'http',
  *   name: 'getUsers',
- * } endpoint - an endpoint argument
+ * }} endpoint - an endpoint argument
  */
 export function queryAPI(endpoint) {
-  if (
-    typeof endpoint === 'object'
-    && endpoint !== null
-    && 'protocol' in endpoint
-    && typeof endpoint.protocol === 'string'
-    && 'name' in endpoint
-    && typeof endpoint.name === 'string'
-  ) {
-    if (!weakMap.has(endpoint)) {
-      weakMap.set(endpoint, 1);
-    } else {
-      const count = weakMap.get(endpoint);
-      if (count >= 5) {
-        throw new Error('Endpoint load is high');
-      } else {
-        weakMap.set(endpoint, count + 1);
-      }
-    }
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
   }
 }
