76fcdb0610a14be5024cf75d8e1ffde1315d658f
Jean Damascene Ngaboyimana <jeandamngaboyimana@gmail.com>
2024-05-20 12:21:34 +0200

    Delete 0x06-unittests_in_js directory
    For Deferment!

--- 0x06-unittests_in_js/5-payment.js
+++
@@ -1,8 +0,0 @@
-const Utils = require('./utils');
-
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const totCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${totCost}`);
-}
-
-module.exports = sendPaymentRequestToApi;
